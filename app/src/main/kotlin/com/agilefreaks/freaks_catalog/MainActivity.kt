f9fc5cc7b76558f463e5173b2a5218f22b681b45
deniscoman <denis.coman13@gmail.com>
2021-07-18 22:24:45 +0300

    Fix formating issues

--- app/src/main/kotlin/com/agilefreaks/freaks_catalog/MainActivity.kt
+++ app/src/main/kotlin/com/agilefreaks/freaks_catalog/MainActivity.kt
@@ -1,13 +1,8 @@
 package com.agilefreaks.freaks_catalog
 
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.FragmentContainerView
-import androidx.navigation.NavController
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
