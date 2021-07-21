964d8f6599792060b94d3e6d3cd818870e28bb73
cipry13 <cipryy_13@yahoo.com>
2021-07-21 13:36:30 +0300

    UI polish

--- features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/FreaksFragment.kt
+++ features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/FreaksFragment.kt
@@ -1,5 +1,6 @@
 package com.agilefreaks.freaks_catalog.features.freaks
 
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -20,11 +21,17 @@ class FreaksFragment : Fragment() {
         viewBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_freaks, container, false)
         viewBinding.lifecycleOwner = viewLifecycleOwner
 
-        val recyclerView = viewBinding.recycleView
-        recyclerView.layoutManager = GridLayoutManager(context, 2)
-
         val myDataset = loadFreaks()
-        recyclerView.adapter = ItemAdapter (this, myDataset)
+
+        val recyclerView = viewBinding.recycleView
+
+        val layoutManager = if(this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+            GridLayoutManager(context, 2)
+        } else
+            GridLayoutManager(context, 3)
+
+        recyclerView.layoutManager = layoutManager
+        recyclerView.adapter = ItemAdapter (myDataset)
 
         return viewBinding.root
     }
@@ -36,8 +43,11 @@ class FreaksFragment : Fragment() {
             Freak("Robert","Solymosi","Android Intern", "Full time", "Beginner", "Description3",0, listOf("Kotlin"), listOf("Freaks Catalog")),
             Freak("Alexandra","Damaschin","Android Developer", "Full time", "Advanced", "Description4",0, listOf("Kotlin, iOS"), listOf("EPIX, ReAsig")),
             Freak("Denis","Coman","Android Developer", "Full time", "Advanced", "Description5",0, listOf("Kotlin, iOS"), listOf("EPIX, ReAsig")),
-            Freak("Alexandru","Calinoiu","Android Developer", "Full time", "Advanced", "Description6",0, listOf("Kotlin, iOS"), listOf("EPIX, ReAsig"))
-
+            Freak("Alexandru","Calinoiu","Android Developer", "Full time", "Advanced", "Description6",0, listOf("Kotlin, iOS"), listOf("EPIX, ReAsig")),
+            Freak("Test1","Test11","Android Intern", "Full time", "Beginner", "Description3",0, listOf("Kotlin"), listOf("Freaks Catalog")),
+            Freak("Test2","Test22","Android Developer", "Full time", "Advanced", "Description4",0, listOf("Kotlin, iOS"), listOf("EPIX, ReAsig")),
+            Freak("Test3","Test33","Android Developer", "Full time", "Advanced", "Description5",0, listOf("Kotlin, iOS"), listOf("EPIX, ReAsig")),
+//            Freak("Test4","Test44","Android Developer", "Full time", "Advanced", "Description6",0, listOf("Kotlin, iOS"), listOf("EPIX, ReAsig"))
         )
     }
 }
