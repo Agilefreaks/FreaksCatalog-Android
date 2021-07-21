024f356a1f13f9551f0ea28795bc9f7591881867
cipry13 <cipryy_13@yahoo.com>
2021-07-21 13:36:30 +0300

    UI polish

--- features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/ItemAdapter.kt
+++ features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/ItemAdapter.kt
@@ -1,5 +1,6 @@
 package com.agilefreaks.freaks_catalog.features.freaks
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -7,7 +8,7 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
-class ItemAdapter(private val context: FreaksFragment, private val dataset: List<Freak>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
+class ItemAdapter(private val dataset: List<Freak>): RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
     class ItemViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
         val textView: TextView = view.findViewById(R.id.item_title)
         val imageView: ImageView = view.findViewById(R.id.item_image)
@@ -20,10 +21,9 @@ class ItemAdapter(private val context: FreaksFragment, private val dataset: List
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
         val item = dataset[position]
-        val nameText: String = item.firstName + " " + item.lastName
-        holder.textView.text = nameText
-        holder.imageView.setImageResource(R.drawable.image512)
-
+        Log.d("Testing", position.toString()  + item.toString());
+        holder.textView.text = item.firstName
+        holder.imageView.setImageResource(R.drawable.testimage)
     }
 
     override fun getItemCount() = dataset.size
