d90ffc315270353b8f37511a388e175979c99a3d
DeeaMatei <mateiandreea1999@yahoo.com>
2021-07-21 13:50:12 +0300

    Error fix

--- features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/FreaksFragment.kt
+++ features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/FreaksFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.agilefreaks.freaks_catalog.features.freaks.databinding.FragmentFreaksBinding
@@ -20,4 +21,9 @@ class FreaksFragment : Fragment() {
         viewBinding.lifecycleOwner = viewLifecycleOwner
         return viewBinding.root
     }
+
+    //val btShow: Button = viewBinding.skillsBtnS
+    //btShow.setOnClickListener {
+
+    //}
 }
