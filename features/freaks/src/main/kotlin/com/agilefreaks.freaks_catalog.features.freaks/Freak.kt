080b7c781a7693de3cf616c7e7305b95a5211417
cipry13 <cipryy_13@yahoo.com>
2021-07-22 11:44:02 +0300

    Code reorganise

--- features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/Freak.kt
+++ features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/Freak.kt
@@ -2,6 +2,9 @@ package com.agilefreaks.freaks_catalog.features.freaks
 
 import java.io.Serializable
 
+/**
+* Freak is the data class that represents the properties of a Freak object.
+ */
 data class Freak(
     val firstName: String,
     val lastName: String,
@@ -12,4 +15,9 @@ data class Freak(
     var image: Int,
     val skills: List<String>,
     val projects: List<String>
-):Serializable
+) : Serializable {
+    // Function for better readability
+    fun printFreak(): String {
+        return "$firstName $lastName \n$role : $norm, $level \n$description \nskills: $skills \nprojects: $projects"
+    }
+}
