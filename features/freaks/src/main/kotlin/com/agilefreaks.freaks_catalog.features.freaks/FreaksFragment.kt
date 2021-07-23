ae491578cfcc28ef0fafcf3f4f2e7b7b83f67290
cipry13 <cipryy_13@yahoo.com>
2021-07-23 16:45:13 +0300

    Different view for tablet

--- features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/FreaksFragment.kt
+++ features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/FreaksFragment.kt
@@ -2,18 +2,20 @@ package com.agilefreaks.freaks_catalog.features.freaks
 
 import android.content.res.Configuration
 import android.os.Bundle
-import android.util.Log
+import android.util.DisplayMetrics
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.agilefreaks.freaks_catalog.features.freaks.databinding.FragmentFreaksBinding
 import com.google.gson.GsonBuilder
 import java.io.*
 import java.util.*
-
+import kotlin.math.pow
+import kotlin.math.sqrt
 
 class FreaksFragment : Fragment() {
     private lateinit var viewBinding: FragmentFreaksBinding
@@ -29,16 +31,36 @@ class FreaksFragment : Fragment() {
         // Initializes data
         val freaksList = loadFreaksFromJson()
 
-        //Splits the screen in 2 columns if the device is in Portrait or in 3 columns otherwise
-        val layoutManager = if(this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
-            GridLayoutManager(context, 2)
-        } else
-            GridLayoutManager(context, 3)
+        // Different UI is device is tablet or phone
+        val layoutManager: RecyclerView.LayoutManager = if(isTablet()) {
+            if (this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                GridLayoutManager(context, 3)
+            } else
+                GridLayoutManager(context, 4)
+        } else {
+            //Splits the screen in 2 columns if the device is in Portrait or in 3 columns otherwise
+            if (this.resources.configuration.orientation == Configuration.ORIENTATION_PORTRAIT) {
+                GridLayoutManager(context, 2)
+            } else
+                GridLayoutManager(context, 3)
+        }
         val recyclerView = viewBinding.recycleView
         recyclerView.layoutManager = layoutManager
         recyclerView.adapter = ItemAdapter (context, freaksList)
 
         return viewBinding.root
+    }
+
+    // Check if device is tablet
+    private fun isTablet(): Boolean {
+        val metrics =  DisplayMetrics()
+        activity?.windowManager?.defaultDisplay?.getMetrics(metrics)
+
+        val widthInches = metrics.widthPixels / metrics.xdpi
+        val heightInches = metrics.heightPixels / metrics.ydpi
+        val diagonalInches = sqrt(widthInches.toDouble().pow(2.0) + heightInches.toDouble().pow(2.0))
+
+        return diagonalInches >= 6.5
     }
 
     // Populate the freakList from a JSON file
@@ -48,8 +70,8 @@ class FreaksFragment : Fragment() {
         val bufferedReader = BufferedReader(inputReader)
         val inputString = bufferedReader.use { it.readText() }
 
-        val gson2 = GsonBuilder().create()
-        return gson2.fromJson(inputString, Array<Freak>::class.java).toList()
+        val gson = GsonBuilder().create()
+        return gson.fromJson(inputString, Array<Freak>::class.java).toList()
     }
 
     // Populates the freakList with dummy data
