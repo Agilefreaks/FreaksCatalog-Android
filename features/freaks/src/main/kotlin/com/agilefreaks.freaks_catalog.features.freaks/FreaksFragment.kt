c9b399722bce1aa3648132c3d69f689908ffc309
DeeaMatei <mateiandreea1999@yahoo.com>
2021-07-23 11:03:07 +0300

    Refactor code

--- features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/FreaksFragment.kt
+++ features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/FreaksFragment.kt
@@ -9,7 +9,6 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.TextView
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.agilefreaks.freaks_catalog.features.freaks.databinding.FragmentFreaksBinding
@@ -26,49 +25,56 @@ class FreaksFragment : Fragment() {
     ): View {
         viewBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_freaks, container, false)
         viewBinding.lifecycleOwner = viewLifecycleOwner
-        val btShow: Button = viewBinding.skillsBtn
-        btShow.setOnClickListener{
-            val dialog = BottomSheetDialog(requireContext())
-            val view = layoutInflater.inflate(R.layout.bottom_sheet_dialog, null)
-            dialog.setCancelable(true)
-            dialog.setContentView(view)
-            dialog.show()
-            var checkedBoxes: List<CheckBox> = listOf()
+        val btShowSkills: Button = viewBinding.skillsBtn
+        val btShowProjects: Button = viewBinding.projectsBtn
+        var btnList: List<Button> = listOf()
+        btnList = btnList.plus(btShowProjects)
+        btnList = btnList.plus(btShowSkills)
 
-            val cb1: CheckBox = view.findViewById(R.id.skill1)
-            val cb2: CheckBox = view.findViewById(R.id.skill2)
-            val cb3: CheckBox = view.findViewById(R.id.skill3)
-            val cb4: CheckBox = view.findViewById(R.id.skill4)
+        for(item in btnList) {
+            item.setOnClickListener {
+                val dialog = BottomSheetDialog(requireContext())
+                val view = layoutInflater.inflate(R.layout.bottom_sheet_dialog, null)
+                dialog.setCancelable(true)
+                dialog.setContentView(view)
+                dialog.show()
+                var checkedBoxes: List<CheckBox> = listOf()
 
-            var allCheckBoxes: List<CheckBox> = listOf()
-            allCheckBoxes = allCheckBoxes.plus(cb1)
-            allCheckBoxes = allCheckBoxes.plus(cb2)
-            allCheckBoxes = allCheckBoxes.plus(cb3)
-            allCheckBoxes = allCheckBoxes.plus(cb4)
+                val cb1: CheckBox = view.findViewById(R.id.skill1)
+                val cb2: CheckBox = view.findViewById(R.id.skill2)
+                val cb3: CheckBox = view.findViewById(R.id.skill3)
+                val cb4: CheckBox = view.findViewById(R.id.skill4)
 
-            for (item in allCheckBoxes) {
-                item.setOnClickListener {
-                    if (item.isChecked){
-                        checkedBoxes = checkedBoxes.plus(item)
+                var allCheckBoxes: List<CheckBox> = listOf()
+                allCheckBoxes = allCheckBoxes.plus(cb1)
+                allCheckBoxes = allCheckBoxes.plus(cb2)
+                allCheckBoxes = allCheckBoxes.plus(cb3)
+                allCheckBoxes = allCheckBoxes.plus(cb4)
+
+                for (item in allCheckBoxes) {
+                    item.setOnClickListener {
+                        if (item.isChecked) {
+                            checkedBoxes = checkedBoxes.plus(item)
+                        }
                     }
                 }
-            }
 
-            val btReset: TextView? = view?.findViewById(R.id.reset)
-            btReset?.setOnClickListener {
-                Log.d("Testing", checkedBoxes.first().toString())
-                checkedBoxes.forEach{
-                    if (it.isChecked){
-                        it.toggle()
+                val btReset: TextView? = view?.findViewById(R.id.reset)
+                btReset?.setOnClickListener {
+                    Log.d("Testing", checkedBoxes.first().toString())
+                    checkedBoxes.forEach {
+                        if (it.isChecked) {
+                            it.toggle()
 
+                        }
                     }
+                    checkedBoxes = listOf()
                 }
-                checkedBoxes = listOf()
-            }
 
-            val btApply: Button? =  view?.findViewById(R.id.apply_btn)
-            btApply?.setOnClickListener {
-                checkedBoxes = listOf()
+                val btApply: Button? = view?.findViewById(R.id.apply_btn)
+                btApply?.setOnClickListener {
+                    checkedBoxes = listOf()
+                }
             }
         }
 
