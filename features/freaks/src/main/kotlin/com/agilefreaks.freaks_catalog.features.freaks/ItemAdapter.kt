5231a1eb19a9e6a9ecfff7e28abd46cabb4a29cd
Ciprian Hotea <cipryy_13@yahoo.com>
2021-07-30 14:52:21 +0300

    Fix lint issues (#8)
    * Fix lint issues
    
    * Fix lint issues
    
    * Pass object from freaks to details
    
    * Fix more lint issues

--- features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/ItemAdapter.kt
+++ features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/ItemAdapter.kt
@@ -1,17 +1,15 @@
 package com.agilefreaks.freaks_catalog.features.freaks
 
-import android.content.Context
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import com.agilefreaks.freaks_catalog.features.freaks.DescriptionClass.Companion.FREAK_KEY
 
-class ItemAdapter(private val context: Context?, private val freaksList: List<Freak>):
+class ItemAdapter(private val freaksList: List<Freak>) :
     RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -20,7 +18,8 @@ class ItemAdapter(private val context: Context?, private val freaksList: List<Fr
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ItemViewHolder {
-        val adapterLayout = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
+        val adapterLayout =
+            LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
         return ItemViewHolder(adapterLayout)
     }
 
@@ -29,8 +28,8 @@ class ItemAdapter(private val context: Context?, private val freaksList: List<Fr
         holder.textView.text = item.firstName
         holder.imageView.setImageResource(R.drawable.testimage)
 
-        holder.imageView.setOnClickListener{
-            holder.itemView.findNavController().navigate(R.id.freak_details)
+        holder.imageView.setOnClickListener {
+            holder.itemView.findNavController().navigate(R.id.freak_details, bundleOf("freak" to item))
         }
     }
 
