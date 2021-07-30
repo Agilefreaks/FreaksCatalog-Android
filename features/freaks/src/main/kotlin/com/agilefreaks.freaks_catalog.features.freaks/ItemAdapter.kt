155f2c2e1cd14426103fc95c8f6e6aea7aca6b06
cipry13 <cipryy_13@yahoo.com>
2021-07-30 14:00:44 +0300

    Fix lint issues

--- features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/ItemAdapter.kt
+++ features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/ItemAdapter.kt
@@ -1,15 +1,15 @@
 package com.agilefreaks.freaks_catalog.features.freaks
 
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
-import androidx.core.os.bundleOf
-import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 
-class ItemAdapter(private val freaksList: List<Freak>, val onItemCLicked: (Freak) -> Unit) :
+class ItemAdapter(private val freaksList: FreaksListQuery.Freaks?, val onItemCLicked: (FreaksListQuery.Node?) -> Unit) :
     RecyclerView.Adapter<ItemAdapter.ItemViewHolder>() {
 
     class ItemViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -24,16 +24,16 @@ class ItemAdapter(private val freaksList: List<Freak>, val onItemCLicked: (Freak
     }
 
     override fun onBindViewHolder(holder: ItemViewHolder, position: Int) {
-        val item = freaksList[position]
-        holder.textView.text = item.firstName
+        val item = freaksList?.nodes?.get(position)
+        val firstName = item?.name?.substring(0, item.name.indexOf(' '));
+        holder.textView.text = firstName
         holder.imageView.setImageResource(R.drawable.testimage)
-
+        val uri: Uri? = Uri.parse(item?.photo?.uri as String?)
+        Picasso.get().load(uri).into(holder.imageView)
         holder.imageView.setOnClickListener {
-            holder.itemView.findNavController()
-                .navigate(R.id.freak_details, bundleOf("freak" to item))
             onItemCLicked(item)
         }
     }
 
-    override fun getItemCount() = freaksList.size
+    override fun getItemCount(): Int = freaksList?.nodes?.size!!
 }
