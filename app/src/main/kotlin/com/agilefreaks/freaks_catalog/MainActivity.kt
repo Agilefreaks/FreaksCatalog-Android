5ed07797f521b58d238b811a97db318259d3c891
SRobert99 <robert.solymosi99@gmail.com>
2021-08-03 16:05:11 +0300

    Add back button, SAB title

--- app/src/main/kotlin/com/agilefreaks/freaks_catalog/MainActivity.kt
+++ app/src/main/kotlin/com/agilefreaks/freaks_catalog/MainActivity.kt
@@ -2,15 +2,27 @@ package com.agilefreaks.freaks_catalog
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController : NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setupNavigation()
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_container) as NavHostFragment
+        // Instantiate the navController using the NavHostFragment
+        navController = navHostFragment.navController
+        // Make sure actions in the ActionBar get propagated to the NavController
+        setupActionBarWithNavController(navController)
     }
 
     private fun setupNavigation() {
@@ -19,5 +31,11 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         val navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         navigation.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
