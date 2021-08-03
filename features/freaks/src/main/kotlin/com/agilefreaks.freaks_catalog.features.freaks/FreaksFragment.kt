dae6a9db9824b46c1bf6cd2a7960f355910cfa89
DeeaMatei <mateiandreea1999@yahoo.com>
2021-08-03 09:58:45 +0300

    Fix lint issues

--- features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/FreaksFragment.kt
+++ features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/FreaksFragment.kt
@@ -73,7 +73,7 @@ class FreaksFragment : Fragment() {
 
             val btApply: Button? = view?.findViewById(R.id.apply_btn)
             btApply?.setOnClickListener {
-                val activeFilter: String
+
             }
 
             dialog.setContentView(view)
