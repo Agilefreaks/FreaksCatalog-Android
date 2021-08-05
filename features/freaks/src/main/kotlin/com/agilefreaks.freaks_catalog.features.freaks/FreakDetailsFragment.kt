fa9c9ec9e2267ea3285523aec1bc26a688049f21
srobert99 <80225992+srobert99@users.noreply.github.com>
2021-08-05 16:54:06 +0300

    Details (#13)
    * Add details screen
    
    Added controller part
    
    Realised controller part
    
    Added controller part
    
    Added controller part
    
    * Added scalable views
    
    * Added scalable views
    
    * Clean code
    
    * Activity to fragment
    
    * Add back button, SAB title
    
    * Clean code
    
    * Fixed hard codede values
    
    * Code review changes

--- features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/FreakDetailsFragment.kt
+++ features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/FreakDetailsFragment.kt
@@ -4,11 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.agilefreaks.freaks_catalog.features.freaks.databinding.FragmentFreakDetailsBinding
 
 class FreakDetailsFragment : Fragment() {
+
     private lateinit var viewBinding: FragmentFreakDetailsBinding
 
     override fun onCreateView(
@@ -16,9 +18,30 @@ class FreakDetailsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        viewBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_freak_details, container, false)
-        viewBinding.lifecycleOwner = viewLifecycleOwner
-
+        viewBinding =
+            DataBindingUtil.inflate(inflater, R.layout.fragment_freak_details, container, false)
+        setViews()
         return viewBinding.root
     }
+
+    private fun setViews() {
+        arguments?.let {
+            val freak = it.getSerializable("freak") as Freak
+            activity
+            viewBinding.descriptionFreak.text = freak.description
+            viewBinding.skillsFreak.text =
+                getString(R.string.skills_template, freak.skills.joinToString(", "))
+            viewBinding.projectFreak.text =
+                getString(R.string.projects_template, freak.projects.joinToString(", "))
+            viewBinding.titleFreak.text = getString(R.string.title_template, freak.role, freak.norm)
+            viewBinding.levelFreak.text = getString(R.string.level_project, freak.level)
+            initBar(freak.firstName,freak.lastName)
+        }
+    }
+
+    fun initBar(firstName:String, lastName:String) {
+        val mainActivity_tool_bar = (activity as AppCompatActivity).supportActionBar
+        mainActivity_tool_bar?.setTitle(getString(R.string.name_template, firstName, lastName))
+    }
 }
+
