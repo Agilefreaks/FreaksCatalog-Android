0c724ec0aafe23957159816134e6eafa81408c11
srobert99 <80225992+srobert99@users.noreply.github.com>
2021-08-05 16:54:06 +0300

    Details (#13)
    * Add details screen
    
    Added controller part
    
    Realised controller part
    
    Added controller part
    
    Added controller part
    
    * Added scalable views
    
    * Added scalable views
    
    * Clean code
    
    * Activity to fragment
    
    * Add back button, SAB title
    
    * Clean code
    
    * Fixed hard codede values
    
    * Code review changes

--- app/src/main/kotlin/com/agilefreaks/freaks_catalog/MainActivity.kt
+++ app/src/main/kotlin/com/agilefreaks/freaks_catalog/MainActivity.kt
@@ -2,11 +2,14 @@ package com.agilefreaks.freaks_catalog
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,5 +22,12 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         val navigation = findViewById<BottomNavigationView>(R.id.bottom_navigation)
         navigation.setupWithNavController(navController)
+        setupActionBarWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_container) as NavHostFragment
+        val navController = navHostFragment.navController
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
