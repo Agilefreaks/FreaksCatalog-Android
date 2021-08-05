37c311c530447b38dd0db0ece86e1454b171904e
Andreea-Maria Matei <59807073+DeeaMatei@users.noreply.github.com>
2021-08-05 15:10:05 +0300

    Origin/filters (#10)
    * Add filter buttons
    
    * Error fix
    
    * Create skill filter UI
    
    * Add Apply Button
    
    * Add checkbox functionalities
    
    * Add horizontal lines
    
    * Fix errors
    
    * Add design elements
    
    * Refactor code
    
    * Add Active filters list
    
    * Adapt code to work with Freak List
    
    * Create FilterAdapter
    
    * Refactor code
    
    * Create RecyclerView for Filters
    
    * Code refactor
    
    * Remove magic number
    
    * Fix detekt issues
    
    * Fix lint issues
    
    * Refactor code
    
    * Implement Reset Button functionalities
    
    * Fix detekt and lint issues
    
    * Fix some PR issues
    
    * Fix some PR issues
    
    * Fix more PR issues
    
    * Fix more PR issues
    
    * Fix reset button spacing
    
    * Update git ignore
    
    * Update git ignore
    
    * Update git ignore
    
    * Fix naming issues

---
+++ features/freaks/src/main/kotlin/com/agilefreaks.freaks_catalog.features.freaks/FilterAdapter.kt
@@ -0,0 +1,37 @@
+package com.agilefreaks.freaks_catalog.features.freaks
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.CheckBox
+import androidx.recyclerview.widget.RecyclerView
+
+class FilterAdapter(private val filters: List<String>) :
+    RecyclerView.Adapter<FilterAdapter.FilterViewHolder>() {
+    var filterReset: Boolean = false
+
+    class FilterViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val filterCheckBox: CheckBox = view.findViewById(R.id.filter_checkbox)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FilterViewHolder {
+        val adapterLayout =
+            LayoutInflater.from(parent.context).inflate(R.layout.filter_item, parent, false)
+        return FilterViewHolder(adapterLayout)
+    }
+
+    override fun onBindViewHolder(holder: FilterViewHolder, position: Int) {
+        val filter = filters[position]
+        holder.filterCheckBox.text = filter
+        if (filterReset) {
+            holder.filterCheckBox.isChecked = false
+        }
+    }
+
+    fun resetCheckboxes(){
+        filterReset = true
+        notifyItemRangeChanged(0, filters.size)
+    }
+
+    override fun getItemCount() = filters.size
+}
