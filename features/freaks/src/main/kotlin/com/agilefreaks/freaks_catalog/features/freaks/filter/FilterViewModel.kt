48df16d346bc3c90c77a01996d5f0530882c9875
cipry13 <cipryy_13@yahoo.com>
2021-08-26 16:37:51 +0300

    Refactor FilterItem from String to Technology and Project

--- features/freaks/src/main/kotlin/com/agilefreaks/freaks_catalog/features/freaks/filter/FilterViewModel.kt
+++ features/freaks/src/main/kotlin/com/agilefreaks/freaks_catalog/features/freaks/filter/FilterViewModel.kt
@@ -5,6 +5,8 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.agilefreaks.freaks_catalog.features.freaks.model.FilterItem
+import com.agilefreaks.freaks_catalog.features.freaks.model.Project
+import com.agilefreaks.freaks_catalog.features.freaks.model.Technology
 import kotlinx.coroutines.launch
 
 class FilterViewModel : ViewModel() {
@@ -34,23 +36,24 @@ class FilterViewModel : ViewModel() {
 
     private fun loadSkills(): MutableList<FilterItem> {
         val technologiesList: MutableList<FilterItem> = mutableListOf()
-        technologiesList.add(FilterItem("1", "Android"))
-        technologiesList.add(FilterItem("2", "Kotlin"))
-        technologiesList.add(FilterItem("3", "iOS"))
-        technologiesList.add(FilterItem("4", "Ruby"))
-        technologiesList.add(FilterItem("5", "QA"))
-        technologiesList.add(FilterItem("6", "Other Skill"))
+        technologiesList.add(Technology("1", "Android"))
+        technologiesList.add(Technology("2", "Kotlin"))
+        technologiesList.add(Technology("3", "iOS"))
+        technologiesList.add(Technology("4", "Ruby"))
+        technologiesList.add(Technology("5", "QA"))
+        technologiesList.add(Technology("6", "Other Skill"))
         technologiesList.sortBy { it.name }
         return technologiesList
     }
 
     private fun loadProjects(): MutableList<FilterItem> {
         val projectsList: MutableList<FilterItem> = mutableListOf()
-        projectsList.add(FilterItem("1", "Freaks Catalog"))
-        projectsList.add(FilterItem("2", "EPIX"))
-        projectsList.add(FilterItem("3", "reAsig"))
-        projectsList.add(FilterItem("4", "New Project"))
+        projectsList.add(Project("1", "Freaks"))
+        projectsList.add(Project("2", "EPIX"))
+        projectsList.add(Project("3", "reAsig"))
+        projectsList.add(Project("4", "New Project"))
         projectsList.sortBy { it.name }
         return projectsList
     }
+
 }
