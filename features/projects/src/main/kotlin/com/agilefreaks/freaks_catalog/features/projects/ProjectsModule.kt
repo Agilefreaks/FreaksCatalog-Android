fb2c6d86bf20cba723a134be8d91bc3a97b5689f
SRobert99 <robert.solymosi99@gmail.com>
2021-08-30 14:35:13 +0300

    Code review changes

--- features/projects/src/main/kotlin/com/agilefreaks/freaks_catalog/features/projects/ProjectsModule.kt
+++ features/projects/src/main/kotlin/com/agilefreaks/freaks_catalog/features/projects/ProjectsModule.kt
@@ -3,7 +3,6 @@ package com.agilefreaks.freaks_catalog.features.projects
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-
 val projectsFragmentModule = module {
     single <ProjectsDataSource> {ProjectsApolloDataSource()}
     single<ProjectsRepository> {ProjectsRepositoryImpl(get())}
