9bbae77f48557df513e89593024f2cd0d965f82c
DeeaMatei <mateiandreea1999@yahoo.com>
2021-09-03 10:33:50 +0300

    Fix project opening

--- features/projects/src/main/kotlin/com/agilefreaks/freaks_catalog/features/projects/ProjectAdapter.kt
+++ features/projects/src/main/kotlin/com/agilefreaks/freaks_catalog/features/projects/ProjectAdapter.kt
@@ -25,7 +25,7 @@ class ProjectAdapter(private val projectsList: List<Project>, private val onItem
         RecyclerView.ViewHolder(binding.root) {
         fun bind(project: Project, onItemCLicked: (Project) -> Unit) {
             binding.project = project
-            binding.projectImage.setOnClickListener {onItemCLicked(project)}
+            binding.projectCardView.setOnClickListener {onItemCLicked(project)}
         }
     }
 }
