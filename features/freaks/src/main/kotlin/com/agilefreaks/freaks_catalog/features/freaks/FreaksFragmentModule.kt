9da29949a04399c8cab431ed3ebeb8ef2b03ed6b
cipry13 <cipryy_13@yahoo.com>
2021-09-06 14:42:55 +0300

    Rebase

--- features/freaks/src/main/kotlin/com/agilefreaks/freaks_catalog/features/freaks/FreaksFragmentModule.kt
+++ features/freaks/src/main/kotlin/com/agilefreaks/freaks_catalog/features/freaks/FreaksFragmentModule.kt
@@ -2,6 +2,7 @@ package com.agilefreaks.freaks_catalog.features.freaks
 
 import com.agilefreaks.freaks_catalog.features.freaks.filter.FilterRepository
 import com.agilefreaks.freaks_catalog.features.freaks.filter.FilterRepositoryImpl
+import com.agilefreaks.freaks_catalog.features.freaks.filter.FilterViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -10,7 +11,9 @@ val freaksFragmentModule = module {
     single<FreaksRepository> { FreaksRepositoryImpl(get()) }
     single<FilterRepository> { FilterRepositoryImpl(get()) }
     viewModel {
-        FreaksViewModel(get())
+        FreaksViewModel(get(), get())
+    }
+    viewModel{
         FilterViewModel()
     }
 }
--- features/freaks/src/main/kotlin/com/agilefreaks/freaks_catalog/features/freaks/details/FreakDetailsRepository.kt
+++ features/freaks/src/main/kotlin/com/agilefreaks/freaks_catalog/features/freaks/details/FreakDetailsRepository.kt
@@ -4,38 +4,30 @@ import com.agilefreaks.freaks_catalog.features.freaks.model.FreakDetails
 import com.agilefreaks.freaks_catalog.features.freaks.FreakDetailsQuery
 
 interface FreakDetailsRepository {
-    suspend fun getFreakFromApi(freakId: String): FreakDetails?
+    suspend fun getFreakDetailsFromApi(freakId: String): FreakDetails?
 }
 
 class FreakDetailsRepositoryImpl(private val dataSource: FreakDetailsDataSource) :
     FreakDetailsRepository {
-    override suspend fun getFreakFromApi(freakId: String): FreakDetails? =
-        mapFreaks(dataSource.getFreaks())?.getFreakById(freakId)
+    override suspend fun getFreakDetailsFromApi(freakId: String): FreakDetails? =
+        dataSource.getFreakDetails(freakId)?.freak?.toFreak()
 
-    private fun mapFreaks(response: FreakDetailsQuery.Data?): List<FreakDetails>? =
-        response?.freaks?.nodes?.map {
-            it.toFreak()
-        }
-
-    private fun List<FreakDetails>.getFreakById(freakId: String): FreakDetails? =
-        this.find { freak -> freak.id == freakId }
-
-    private fun FreakDetailsQuery.Node?.toFreak() = FreakDetails(
-        id = this?.id ?: "",
-        firstName = this?.firstName ?: "",
-        lastName = this?.lastName ?: "",
-        description = this?.description ?: "",
-        level = this?.level?.name ?: "",
-        norm = this?.norm?.name ?: "",
-        photo = this?.photo?.uri as String? ?: "",
-        role = this?.role?.name ?: "",
-        projects = buildProjectsNameList(this!!.projects).joinToString(", "),
-        skills = buildSkillsNameList(this.skills).joinToString(", ")
+    private fun FreakDetailsQuery.Freak.toFreak() = FreakDetails(
+        id = this.id,
+        firstName = this.firstName,
+        lastName = this.lastName,
+        description = this.description,
+        level = this.level.name,
+        norm = this.norm.name,
+        photo = this.photo?.uri as String? ?: "",
+        role = this.role.name,
+        projects = buildProjectsNameList(this.projects).joinToString(", "),
+        skills = buildSkillsNameList(this.technologies).joinToString(", ")
     )
 
     private fun buildProjectsNameList(projects: List<FreakDetailsQuery.Project>): List<String> =
         projects.map { it.name }
 
-    private fun buildSkillsNameList(skills: List<FreakDetailsQuery.Skill>): List<String> =
+    private fun buildSkillsNameList(skills: List<FreakDetailsQuery.Technology>): List<String> =
         skills.map { it.name }
 }
